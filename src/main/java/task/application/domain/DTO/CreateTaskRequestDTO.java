ee3731f07b6887d92da687cd63154bf4a8fb2626
Careyogola <ogolacarey254@gmail.com>
2026-06-24 12:59:14 +0300

    feat:dto

---
+++ src/main/java/task/application/domain/DTO/CreateTaskRequestDTO.java
@@ -0,0 +1,21 @@
+
+package task.application.domain.DTO;
+
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import task.application.domain.entity.TaskPriority;
+
+public record CreateTaskRequestDTO(
+    @NotBlank(message = ERROR_MESSAGE_TITLE_LENGTH)
+    @Length(max = 255, message = ERROR_MESSAGE_TITLE_LENGTH)
+    String title, 
+    String description, 
+    LocalDate duDate, 
+    TaskPriority priority) {
+
+        private static final String ERROR_MESSAGE_TITLE_LENGTH = "Title must be between 1 and 255 characters";
+
+}
